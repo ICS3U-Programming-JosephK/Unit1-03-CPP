5e8c6115cad7e7526a49d17d057f56c22fbe9124
JosephK2006 <jiyong.kwon@stu.ocsb.ca>
2022-09-16 02:18:13 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Ms Raffin All rights reserved.
+// .
+// Created by: Ms Raffin and borrowed by Joseph
+// Date: Sep. 15, 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is hard." << std::endl;
+}
